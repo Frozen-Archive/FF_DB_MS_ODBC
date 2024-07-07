03b21678e077f58371a2148526a36b977a11c166
Frozen Forest Reality Technologies <frozenforeststudio@gmail.com>
2024-07-07 22:06:08 +0300

    Read Me and Includes Update

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # FF_DB_MS_ODBC
+This is an ODBC plugin for Unreal Engine 5 and Windows platform.
+
+# THIRD PARTY LIBRARY
+We used Microsoft Windows C++ ODBC API
  
--- Source/FF_DB_MS_ODBC/Public/MS_ODBC_Includes.h
+++ Source/FF_DB_MS_ODBC/Public/MS_ODBC_Includes.h
@@ -1,14 +1,14 @@
 #pragma once
 
+#include "Kismet/KismetMathLibrary.h"
+#include "Kismet/KismetStringLibrary.h"
+
 THIRD_PARTY_INCLUDES_START
 
 #define WIN32_LEAN_AND_MEAN
 #include "Windows/AllowWindowsPlatformTypes.h"
 #include <sqlext.h>
 #include "Windows/HideWindowsPlatformTypes.h"
-
-#include "Kismet/KismetMathLibrary.h"
-#include "Kismet/KismetStringLibrary.h"
 
 #include <string>
 
