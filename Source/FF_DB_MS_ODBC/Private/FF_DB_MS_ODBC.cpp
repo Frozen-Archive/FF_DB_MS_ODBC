0309e4e356d904a63da8ceb6556f2b33ee81acb4
Frozen Forest Reality Technologies <frozenforeststudio@gmail.com>
2024-07-18 00:32:59 +0300

    Update FF_DB_MS_ODBC.cpp

--- Source/FF_DB_MS_ODBC/Private/FF_DB_MS_ODBC.cpp
+++ Source/FF_DB_MS_ODBC/Private/FF_DB_MS_ODBC.cpp
@@ -1,19 +1,17 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "FF_DB_MS_ODBC.h"
-#include "Interfaces/IPluginManager.h"
 
 #define LOCTEXT_NAMESPACE "FFF_DB_MS_ODBCModule"
 
 void FFF_DB_MS_ODBCModule::StartupModule()
 {
-	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+
 }
 
 void FFF_DB_MS_ODBCModule::ShutdownModule()
 {
-	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
-	// we call this function before unloading the module.
+
 }
 
 #undef LOCTEXT_NAMESPACE
